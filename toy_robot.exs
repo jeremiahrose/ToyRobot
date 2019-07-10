c382ad736dd2cc5421b4548c66205ff4b69825d3
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2019-07-10 14:14:03 +1000

    Add alternative move/1 function and new rotate/2 function with tests.
    Changes the direction from atoms (e.g `:north`) to numbers `0-3` so simple geometry
    can be used to move and change direction.

--- toy_robot.exs
+++ toy_robot.exs
@@ -10,8 +10,15 @@ defmodule ToyRobot do
     # e.g x_max of 4 creates 5 horizontal squares
     @x_max 4
     @y_max 4
-    # Direction the robot can face
-    @directions [:north, :south, :east, :west]
+    # The directions the robot can face are numbered 0-3 in anticlockwise order.
+    # This allows to easily change direction by adding or subtracting, and to easily
+    # add new directions (e.g @north_west 1.5) if we so desired.
+    @east 0
+    @north 1
+    @west 2
+    @south 3
+    # Allowable directions
+    @directions 0..3
 
     @doc """
     Given a tuple representing the robot's state, return the same tuple if the state is valid,
@@ -35,10 +42,23 @@ defmodule ToyRobot do
     Alter the given state so that the robot moves one square in the direction it is facing.
     If the robot is facing the edge of the board, it is not moved
     """
-    def move({x,y,:north}) when y<@y_max, do: {x,y+1,:north}
-    def move({x,y,:south}) when y>0, do: {x,y-1,:south}
-    def move({x,y,:east}) when x<@x_max, do: {x+1,y,:east}
-    def move({x,y,:west}) when x>0, do: {x-1,y,:west}
+    def move({x,y,@north}) when y<@y_max, do: {x,y+1,@north}
+    def move({x,y,@south}) when y>0, do: {x,y-1,@south}
+    def move({x,y,@east}) when x<@x_max, do: {x+1,y,@east}
+    def move({x,y,@west}) when x>0, do: {x-1,y,@west}
     def move(state_facing_edge), do: state_facing_edge
 
+    # Alternative move that would also work diagonally.
+    #def move({x, y, f}) do
+    #    {dx, dy} = {round(:math.cos(f*:math.pi/2)), round(:math.sin(f*:math.pi/2))}
+    #    check_state({x + dx, y + dy, f}) || {x , y, f}
+    #end
+
+    @doc """
+    Alter the given state so that the robot rotates left or right.
+
+    The rotation parameter must be -1 or 1 for a right or left hand turn respectively.
+    """
+    def rotate({x,y,f}, r) when r in [-1,1], do: {x,y,Integer.mod(f+r,4)}    
+   
 end
--- toy_robot_test.exs
+++ toy_robot_test.exs
@@ -7,31 +7,39 @@ defmodule ToyRobotTest do
     use ExUnit.Case
 
     test "Check valid state" do
-        assert ToyRobot.check_state({0,0,:north}) == {0,0,:north}
+        assert ToyRobot.check_state({0,0,1}) == {0,0,1}
     end
 
     test "Check invalid position" do
-        assert ToyRobot.check_state({0,-1,:north}) == nil
+        assert ToyRobot.check_state({0,-1,1}) == nil
     end
 
     test "Check invalid direction" do
-        assert ToyRobot.check_state({0,0,2}) == nil
+        assert ToyRobot.check_state({0,0,"northh"}) == nil
     end
 
     test "Face valid direction" do
-        assert ToyRobot.face({0,0,:west}, :east) == {0,0,:east}
+        assert ToyRobot.face({0,0,2}, 0) == {0,0,0}
     end
 
     test "Face invalid direction" do
-        assert ToyRobot.face({0,0,:west}, 8768) == nil
+        assert ToyRobot.face({0,0,2}, 8768) == nil
     end
 
     test "Move one square north" do
-        assert ToyRobot.move({0,0,:north}) == {0,1,:north}
+        assert ToyRobot.move({0,0,1}) == {0,1,1}
     end
 
     test "Don't move off edge" do
-        assert ToyRobot.move({0,0,:west}) == {0,0,:west}
+        assert ToyRobot.move({0,0,2}) == {0,0,2}
+    end
+
+    test "Rotate past zero" do
+        assert ToyRobot.rotate({0,0,0}, -1) == {0,0,3}
+    end
+
+    test "Rotate left" do
+        assert ToyRobot.rotate({2,3,1}, 1) == {2,3,2}
     end
         
 end
